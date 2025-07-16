62980963cfd90c47add049bab4382e6bb26f8431
Mehdi Roualy <roualymehdi5@gmail.com>
2025-07-16 17:00:00 +0200

    health + bossAttack(debut)

---
+++ Assets/script/Player/Health.cs
@@ -0,0 +1,35 @@
+using System;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    private int maxHeart = 5;
+    public int Heart;
+    [SerializeField] private bool isNoHit;
+    void Start()
+    {
+        Heart = 3;
+        isNoHit = true;
+    }
+    public void DamageTaken(int nbDamage){
+        Heart -= nbDamage;
+        isNoHit = false;
+        Heart = Mathf.Clamp(Heart, 0, maxHeart);
+        if (Heart == 0)
+        {
+            playerDeath();
+        }
+    }
+    public bool IsDead(){
+        if (Heart == 0)
+        {
+            return true;
+        }
+        return false;
+    }
+    private void playerDeath()
+    {
+        Debug.Log("player is death");
+        Destroy(gameObject);
+    }
+}
